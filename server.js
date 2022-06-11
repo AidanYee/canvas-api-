f7a8a8365e83dcecec6f8340e3d96d2897bdec5b
Mitchell Schofield <scho.mitch@gmail.com>
2022-06-11 15:22:00 -0700

    this coomit is just before pulling aidens updated work on server

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ db.connect();
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan("dev"));
+ app.use(morgan("dev"));
 
-app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  "/styles",
-  sassMiddleware({
-    source: __dirname + "/styles",
-    destination: __dirname + "/public/styles",
-    isSass: false, // false => scss, true => sass
-  })
-);
+// app.use(
+//   "/styles",
+//   sassMiddleware({
+//     source: __dirname + "/styles",
+//     destination: __dirname + "/public/styles",
+//     isSass: false, // false => scss, true => sass
+//   })
+// );
 
 app.use(express.static("public"));
 
