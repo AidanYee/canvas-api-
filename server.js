0fbfd4324c86bc6d344a73e4634edc1b465760bd
AidanYee <aidan.yee@pm.me>
2022-06-11 15:17:57 -0700

    connect to ElephantSQL DB. Clean up skeleton. Working schema & seeds.

--- server.js
+++ server.js
@@ -1,47 +1,37 @@
-// load .env data into process.env
+// load .env data into process.env, allows us to use env files
 require("dotenv").config();
+
+// -allows us to make requests. CORS allows the server to explicitly whitelist certain origin and help to bypass the same-origin policy
+const cors = require("cors");
 
 // Web server config
 const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
+
 const express = require("express");
 const app = express();
+
+// -morgan is the server listner, allows us to receives server based errors (dev tool)
 const morgan = require("morgan");
 
-// PG database client/connection setup
-const { Client } = require("pg");
-const dbParams = require("./lib/db.js");
-const db = new Client(dbParams);
-db.connect();
+// this brings in our elephant sql file, so that we can make the DB connection
+require("./elephantsql");
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// -this calls/ fires up morgan. Logger middleware will generate a detailed log using what is called the default format.
 app.use(morgan("dev"));
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 
-app.use(
-  "/styles",
-  sassMiddleware({
-    source: __dirname + "/styles",
-    destination: __dirname + "/public/styles",
-    isSass: false, // false => scss, true => sass
-  })
-);
-
-app.use(express.static("public"));
+app.use(cors());
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+// const usersRoutes = require("./routes/users");
+// const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+// app.use("/api/users", usersRoutes(db));
+// app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -49,7 +39,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.json({message: "hello world"});
 });
 
 app.listen(PORT, () => {
