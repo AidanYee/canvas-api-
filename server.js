94907a32d888586308ecea621e70bbeb26d570f9
Nicolas <nchlsreimer@gmail.com>
2022-06-12 13:26:46 -0700

    refactored code using router

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// SERVER FILE:
+//------------------------------------------------------------------------------
 // load .env data into process.env, allows us to use env files
 require("dotenv").config();
-const db = require('./elephantsql.js')
+
+const db = require("./elephantsql.js");
 // -allows us to make requests. CORS allows the server to explicitly whitelist certain origin and help to bypass the same-origin policy
 const cors = require("cors");
 
@@ -13,8 +16,6 @@ const app = express();
 // -morgan is the server listner, allows us to receives server based errors (dev tool)
 const morgan = require("morgan");
 
-
-
 // -this calls/ fires up morgan. Logger middleware will generate a detailed log using what is called the default format.
 app.use(morgan("dev"));
 
@@ -22,17 +23,35 @@ app.use(express.json({ extended: true }));
 
 app.use(cors());
 
-
-// this brings in our elephant sql file, so that we can make the DB connection
-//-----------------------------------------------------
+//--------------------------------------------------------------------------------
 // CONNECTS TO SEPERATED ROUTE IN ROUTES FOLDER
+const drawingsRoutes = require("./routes/drawings");
+app.use("/drawings", drawingsRoutes(db));
 
-// const drawingsRoutes = require("./routes/drawings");
-// app.use("/", drawingsRoutes(db));
+//--------------------------------------------------------------------------------
+// FIRST SUCCESSFULL ROUTE:
+// -built with mentor, and proven successful
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
+});
 
-// app.use("./drawings", function (req, res, next) {
-//   res.send("Hello World");
-// });
+//------------------------------------------------------------------------------
+// 404 ERROR HANDLING:
+// - this route is incase someone tries to get to a route that doesn't exist it send them a 404 error
+app.get("*", (req, res) => {
+  res.redirect(404, "/");
+});
+
+//------------------------------------------------------------------------------
+// - It specifies the port on which we want our app to listen
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+//------------------------------------------------------------------------------
+// -We established the connection to express by attaching it to a var named app here is server.js
+//  we export that var so other can use the connection
+module.exports = app;
 
 //-----------------------------------------------------
 // ORIGINAL NOTES FROM BASE GIT HUB REPO
@@ -50,44 +69,3 @@ app.use(cors());
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-
-
-//-----------------------------------------------------
-// FIRST SUCCESSFULL ROUTE:
-// -built with mentor, and proven successful
-app.get("/", (req, res) => {
-  res.json({ message: "hello world poop" });
-});
-
-//---------------------------------------------------------------------------
-// POST ROUTE (connected to save function in canvas app)
-// -this works up until the db insert, then it fails rather agressively
-app.post("/drawings", (req, res) => {
-  const poop = JSON.stringify(req.body);
-  console.log("Points to be saved", poop);
-
-  return db.query(
-    `
-          INSERT INTO drawings (users_id, drawing_name, drawing_points, is_showcase)
-          VALUES (1,'test1','${poop}',false)
-
-        `
-  )
-
-    .then((res) => {
-      console.log("no error from server about query")
-      console.log("res ===>", res);
-      return res;
-      //
-    })
-    .catch(err => console.log(err.message))
-});
-// - this route is incase someone tries to get to a route that doesn't exist it send them a 404 error
-app.get('*', (req, res) => {
-  res.redirect(404, "/");
-});
-
-//------------------------------------------------------------------------------
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
