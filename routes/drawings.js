4ad4a936510e8fb95f0bef68caeb822c0c86a2dd
Nicolas <nchlsreimer@gmail.com>
2022-06-15 18:14:57 -0700

    delete drawings route was added to routes/drawings.js

--- routes/drawings.js
+++ routes/drawings.js
@@ -12,7 +12,7 @@ const router = require("express").Router();
 //  the screen
 
 module.exports = (db) => {
-  //POST drawings
+  //POST drawings ---------------------------------------------------------------------
   router.post("/", (req, res) => {
     const drawingsData = JSON.stringify(req.body.latLong);
     const name = req.body.name;
@@ -20,15 +20,32 @@ module.exports = (db) => {
       .query(
         `
           INSERT INTO drawings (users_id, drawing_name, drawing_points, is_showcase)
-          VALUES (1, $1, $2, false) RETURNING *;`,
+          VALUES (1, $1, $2, true) RETURNING *;`,
         [name, drawingsData]
       )
       .then((response) => {
-        console.log("res ===>", response.rows[0].drawing_points);
+        //console.log("res ===>", response.rows[0].drawing_points);
         return res.send(response.rows);
       })
       .catch((err) => console.log(err.message));
   });
 
+  //--------------------------------------------------------------------------------------
+  // DELETE DRAWING BY ID:
+  router.delete("/:id", (req, res) => {
+    console.log("delete request made", req.body.id);
+    const id = req.params.id;
+
+    return db
+      .query(`DELETE FROM drawings WHERE id = ${id};`)
+
+      .then((response) => {
+        console.log("res delete===>", response);
+        return res.send(response);
+      })
+      .catch((err) => console.log(err.message));
+  });
+
+  //------------
   return router;
 };
